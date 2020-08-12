56222a46575c3a7627f9f6b2604d06e34f099ac9
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-08-12 16:25:32 -0400

    commit without styling

--- api.js
+++ api.js
@@ -1,17 +1,39 @@
-const baseUrl= "https://ghibliapi.herokuapp.com";
+const baseUrl = "https://ghibliapi.herokuapp.com/films";
 
-const sectionOne = document.querySelector(".section");
-const sectionTwo = document.querySelector("#div");
+const sectionOne = document.querySelector("section");
 
-function fetchSpace(){
-    event.preventDefault();
-    fetch(baseUrl)
-    .then((result)=>{
-        return result.json();
-    })
-    .then((data)=> {
-        console.log(data);
-    })
-    .catch ((error)=> console.log(error));
+fetch(baseUrl)
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    displayMovies(data);
+  })
+  .catch((error) => console.log(error));
 
+function displayMovies(movieData) {
+  movieData.forEach((element) => {
+    const flashcard = document.createElement("div"); // created a div with in section class from html
+    flashcard.setAttribute("class", "content"); // gave it a class and name
+
+    // creating diffrent type of tags to display the information
+    const titleInfo = document.createElement("h2");
+    const directorInfo = document.createElement("h3");
+    const releaseInfo = document.createElement("h4");
+    const discriptionInfo = document.createElement("p");
+
+    //connection the above variable/tags 
+    titleInfo.textContent = element.title;
+    directorInfo.textContent = element.director;
+    releaseInfo.textContent = element.release_date;
+    discriptionInfo.textContent = element.description;
+
+    sectionOne.appendChild(flashcard);
+    flashcard.appendChild(titleInfo);
+    flashcard.appendChild(directorInfo);
+    flashcard.appendChild(releaseInfo);
+    flashcard.appendChild(discriptionInfo);
+
+
+  });
 }
