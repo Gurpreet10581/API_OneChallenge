270f5a24d9e6e883c799e0924eeaf8a6a2909759
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-08-12 10:11:06 -0400

    adding asset folder

--- api.js
+++ api.js
@@ -1,1 +1,17 @@
-console.log('It is working!')
+const baseUrl= "https://ghibliapi.herokuapp.com";
+
+const sectionOne = document.querySelector(".section");
+const sectionTwo = document.querySelector("#div");
+
+function fetchSpace(){
+    event.preventDefault();
+    fetch(baseUrl)
+    .then((result)=>{
+        return result.json();
+    })
+    .then((data)=> {
+        console.log(data);
+    })
+    .catch ((error)=> console.log(error));
+
+}
